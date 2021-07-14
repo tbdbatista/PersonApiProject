93d58d7ec134a8c765c4f93fd29e5ffd0d888bcc
tbdbatista <tbdbatista@gmail.com>
2021-07-13 22:30:39 -0300

    Creating entitys -> person and phone, also enums -> PhoneType / Added lombock rules for person and phone classes.

---
+++ src/main/java/one/digitalinnovation/personapi/entitys/Person.java
@@ -0,0 +1,38 @@
+package one.digitalinnovation.personapi.entitys;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    private LocalDate birthDate;
+
+//    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+//    private List<Phone> phones;
+
+
+
+}
